08b00a3190119f2d843a68cfa85376c4c1950552
Chaitanya Deepthi Kaki <68332814+deepthi-kk@users.noreply.github.com>
2023-04-21 16:20:35 -0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import firebase from "firebase";
 
 const firebaseConfig = {
   
-    apiKey: "AIzaSyCGIYqITn8fMnqKmdo9cVuf1JoJ7crbi4U",
+    apiKey: "Your_APIKey",
     authDomain: "visualailogin.firebaseapp.com",
     projectId: "visualailogin",
     storageBucket: "visualailogin.appspot.com",
-    messagingSenderId: "884762503652",
-    appId: "1:884762503652:web:661c00ce8b9a9a9dee3ff1",
+    messagingSenderId: "dummy",
+    appId: "dummy",
     measurementId: "G-CHS8X1X9E1"
   
   };
